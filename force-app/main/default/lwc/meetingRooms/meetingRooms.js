a5b20878dc0e729eed6b60c57a236304af15270e
Glory to Hanuman <gangineni.info@gmail.com>
2023-08-28 16:42:25 +0530

    push

--- force-app/main/default/lwc/meetingRooms/meetingRooms.js
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -2,11 +2,15 @@ import { LightningElement } from 'lwc';
 
 export default class MeetingRooms extends LightningElement {
 
-    metingRooms =[
-        {roomName:'A-01',roomCapacity: '10'},
-        {roomName:'b-01',roomCapacity: '20'},
-        {roomName:'c-01',roomCapacity: '11'},
-        {roomName:'d-01',roomCapacity: '12'},
-        {roomName:'e-01',roomCapacity: '15'},
-    ]
+    meetingRoomsInfo = [
+        {roomName:'A-01', roomCapacity:'12'},
+        {roomName:'A-02', roomCapacity:'16'},
+        {roomName:'A-03', roomCapacity:'12'},
+        {roomName:'B-01', roomCapacity:'5'},
+        {roomName:'B-02', roomCapacity:'8'},
+        {roomName:'B-03', roomCapacity:'10'},
+        {roomName:'C-01', roomCapacity:'20'}
+
+    ];
+   
 }
